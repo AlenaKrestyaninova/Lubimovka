c59d6c71de38f8767a6959a7646d1274ba10fc04
Arina Kostrova <arinakostrova@yandex.ru>
2022-10-24 20:16:08 +0400

    feat: stylized and js added

---
+++ scripts/persons.js
@@ -0,0 +1,22 @@
+const persons = [
+    {
+        name: 'Мария Крупник',
+        description: 'Арт-менеджер, переводчик, куратор образовательной программы',
+        photo: '../images/persons/krupnik.webp'
+    },
+    {
+        name: 'Вероника Устимук',
+        description: 'Продюсер, координатор волонтёров фестиваля',
+        photo: '../images/persons/Ustimuk_cut.webp'
+    },
+    {
+        name: 'Мария Крупник',
+        description: 'Арт-менеджер, переводчик, куратор образовательной программы',
+        photo: '../images/persons/krupnik.webp'
+    },
+    {
+        name: 'Мария Крупник',
+        description: 'Арт-менеджер, переводчик, куратор образовательной программы',
+        photo: '../images/persons/krupnik.webp'
+    }
+]
